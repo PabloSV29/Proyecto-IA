f6254300cca22c834566f97caca440295dc416d2
NachoCHR <jchavarria2018@udec.cl>
2024-06-08 20:54:23 -0400

    Funcion para verificar quien gano funcionando

--- conecta4/main.py
+++ conecta4/main.py
@@ -3,120 +3,48 @@ import numpy as np
 from random import *
 from os import system
 
-def DFSL_gano(tablero, player, posicion, limite, acumulado):
-    pos_y, pos_x = posicion
-    print(f"La posicion: {pos_y,pos_x}. El limite es {limite} y el acumulado es {acumulado}")
-    # Verificar que la posicion en donde estamos es una posicion correcta.
-    if pos_y < 0 or pos_y >= len(tablero) or pos_x < 0 or pos_x >= len(tablero[0]) or limite < 0:
-        print("Posicion Incorrecta. No cumple con alguno de los requerimientos")
-        return None
-    # Verificar caso base, es decir, que se hayan encontrado 4 fichas acumuladas.
-    if player == 'jugador_uno':
-        print("-----> JUGADOR UNO") 
-        if tablero[pos_y, pos_x] == 1:
-            acumulado = acumulado + 1 
-            if acumulado == 3:
-                return 'WIN'
-            # Verificar arriba.
-            print("## Verificar posicion arriba")
-            DFSL_gano(tablero, 'jugador_uno', (pos_y - 1, pos_x), limite - 1, acumulado)
-            # Verificar diagonal izquierda superior.
-            print("## Verificar diagonal superior izquierda")
-            DFSL_gano(tablero, 'jugador_uno', (pos_y - 1, pos_x - 1), limite - 1, acumulado)
-            # Verificamos izquierda.
-            print("## Verificar izquierda")
-            DFSL_gano(tablero, 'jugador_uno', (pos_y, pos_x - 1), limite - 1, acumulado)
-            # Verificamos diagonal izquierda inferior.
-            print("## Verificar diagonal inferior izquierda")
-            DFSL_gano(tablero, 'jugador_uno', (pos_y + 1, pos_x - 1), limite - 1, acumulado)
-            # Verificamos abajo.
-            print("## Verificar abajo")
-            DFSL_gano(tablero, 'jugador_uno', (pos_y + 1, pos_x), limite - 1, acumulado)
-            # Verificamos diagonal derecha inferior.
-            print("## Verificar diagonal inferior derecha")
-            DFSL_gano(tablero, 'jugador_uno', (pos_y + 1, pos_x + 1), limite - 1, acumulado)
-            # Verificamos derecha.
-            print("## Verificar derecha")
-            DFSL_gano(tablero, 'jugador_uno', (pos_y, pos_x + 1), limite - 1, acumulado)
-            # Verficamos diagonal derecha superior.
-            print("## Verificar diagonal superior derecha")
-            DFSL_gano(tablero, 'jugador_uno', (pos_y - 1, pos_x + 1), limite - 1, acumulado)
-        else:
-            return None
+def gano(tablero, jugador):
 
-    elif player == 'jugador_dos':
-        print("-----> JUGADOR DOS") 
-        if tablero[pos_y, pos_x] != 2: 
-            return None
-        else:
-            acumulado = acumulado + 1 
-            if acumulado == 4:
-                return 'WIN'
-            else:
-                # Verificar arriba.
-                print("## Verificar posicion arriba")
-                DFSL_gano(tablero, 'jugador_dos', (pos_y - 1, pos_x), limite - 1, acumulado)
-                # Verificar diagonal izquierda superior.
-                print("## Verificar diagonal superior izquierda")
-                DFSL_gano(tablero, 'jugador_dos', (pos_y - 1, pos_x - 1), limite - 1, acumulado)
-                # Verificamos izquierda.
-                print("## Verificar izquierda")
-                DFSL_gano(tablero, 'jugador_dos', (pos_y, pos_x - 1), limite - 1, acumulado)
-                # Verificamos diagonal izquierda inferior.
-                print("## Verificar diagonal inferior izquierda")
-                DFSL_gano(tablero, 'jugador_dos', (pos_y + 1, pos_x - 1), limite - 1, acumulado)
-                # Verificamos abajo.
-                print("## Verificar abajo")
-                DFSL_gano(tablero, 'jugador_dos', (pos_y + 1, pos_x), limite - 1, acumulado)
-                # Verificamos diagonal derecha inferior.
-                print("## Verificar diagonal inferior derecha")
-                DFSL_gano(tablero, 'jugador_dos', (pos_y + 1, pos_x + 1), limite - 1, acumulado)
-                # Verificamos derecha.
-                print("## Verificar derecha")
-                DFSL_gano(tablero, 'jugador_dos', (pos_y, pos_x + 1), limite - 1, acumulado)
-                # Verficamos diagonal derecha superior.
-                print("## Verificar diagonal superior derecha")
-                DFSL_gano(tablero, 'jugador_dos', (pos_y - 1, pos_x + 1), limite - 1, acumulado)
-    return None
+    numFil = len(tablero)
+    numCol = len(tablero[0])
+
+    if jugador == 'jugador_uno':
+        valor_ficha = 1
+    elif jugador == 'jugador_dos':
+        valor_ficha = 2
+
+    # Verificar las horizontales
+    for fila in range(numFil):
+        for col in range(numCol - 3):
+            if tablero[fila][col] == valor_ficha and tablero[fila][col + 1] == valor_ficha and tablero[fila][col + 2] == valor_ficha and tablero[fila][col + 3] == valor_ficha:
+                return True 
+
+    # Verificar verticales
+    for fila in range(numFil - 3):
+        for col in range(numCol):
+            if tablero[fila][col] == valor_ficha and tablero[fila + 1][col] == valor_ficha and tablero[fila + 2][col] == valor_ficha and tablero[fila + 3][col] == valor_ficha:
+                return True 
     
+    # Verificar diagonal hacia der.
+    for fila in range(numFil - 3):
+        for col in range(numCol - 3):
+            if tablero[fila][col] == valor_ficha and tablero[fila + 1][col + 1] == valor_ficha and tablero[fila + 2][col + 2] == valor_ficha and tablero[fila + 3][col + 3] == valor_ficha:
+                return True
 
-def DFSL_ganoo(tablero, player, posicion, limite, acumulado):
-    pos_y, pos_x = posicion
-    print(f"La posicion: {pos_y,pos_x}. El limite es {limite} y el acumulado es {acumulado}")
-    
-    # Verificar que la posicion en donde estamos es una posicion correcta.
-    if pos_y < 0 or pos_y >= len(tablero) or pos_x < 0 or pos_x >= len(tablero[0]) or limite < 0:
-        print("Posicion Incorrecta. No cumple con alguno de los requerimientos")
-        return None
+    # Verificar diagonal hacia izq.
+    for fila in range(3, numFil):
+        for col in range(numCol - 3):
+            if tablero[fila][col] == valor_ficha and tablero[fila - 1][col + 1] == valor_ficha and tablero[fila - 2][col + 2] == valor_ficha and tablero[fila - 3][col + 3] == valor_ficha:
+                return True
 
-    # Verificar el jugador actual y la condición de victoria.
-    if player == 'jugador_uno':
-        print("-----> JUGADOR UNO")
-        if tablero[pos_y][pos_x] != 1:
-            return None
-        else:
-            acumulado += 1
-            if acumulado == 4:
-                return 'WIN'
-    elif player == 'jugador_dos':
-        print("-----> JUGADOR DOS")
-        if tablero[pos_y][pos_x] != 2:
-            return None
-        else:
-            acumulado += 1
-            if acumulado == 4:
-                return 'WIN'
 
-    # Verificar en todas las direcciones.
-    directions = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
-    for dy, dx in directions:
-        new_pos = (pos_y + dy, pos_x + dx)
-        result = DFSL_gano(tablero, player, new_pos, limite - 1, acumulado)
-        if result == 'WIN':
-            return 'WIN'
-    
-    return None
+    return False
 
+def dibujar_tablero(tablero):
+    for fila in tablero:
+        for valor in fila:
+            print("\t", valor, end=" ")
+        print()
 
 def modificar_tablero(tablero,columna, player):
     posicion = (0,0) 
@@ -134,12 +62,6 @@ def modificar_tablero(tablero,columna, player):
     return none
 
 
-def dibujar_tablero(tablero):
-    for fila in tablero:
-        for valor in fila:
-            print("\t", valor, end=" ")
-        print()
-
 def main():
     
     tablero = np.zeros((6,7), dtype=int)
@@ -156,8 +78,7 @@ def main():
         posicion = modificar_tablero(tablero, numero, 'jugador_uno')
         print(posicion)
         # system("clear")
-        ss = DFSL_ganoo(tablero, 'jugador_uno', posicion,4,0)
-        if ss == 'WIN': 
+        if gano(tablero, 'jugador_uno'):
             dibujar_tablero(tablero)
             print(jugador_uno + " GANADOR!")
             break
@@ -168,10 +89,9 @@ def main():
         dibujar_tablero(tablero)
         variable = int(input())
         posicion = modificar_tablero(tablero, variable, 'jugador_dos')
-        ss = DFSL_ganoo(tablero, 'jugador_dos', posicion,4,0)
-        if ss == 'WIN':
+        if gano(tablero, 'jugador_dos'):
             dibujar_tablero(tablero)
-            print(jugador_uno + " GANADOR!")
+            print(jugador_dos + " GANADOR!")
             break
         # system("clear")
 main()
